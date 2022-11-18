b9f8a6c293287ff4b44651977f918020f10243db
João Benine <joaobenine@gmmail.com>
2022-11-18 02:08:02 -0300

    metod create new date

--- src/script.js
+++ src/script.js
@@ -2,6 +2,11 @@
      constructor(dataFutura) {
          this.dataFutura = dataFutura;
      }
+
+     get _futureDate() {
+         return new(this._futureDate);
+     }
  }
 
- const dataReferenciaCoutdown = new Countdown('25 December 2022');
+ const dataReferenciaCoutdown = new Countdown('24 December 2022 23:59:59 GMT-0300');
+ console.log(dataReferenciaCoutdown);
