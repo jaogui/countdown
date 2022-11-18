7f71902e8c7d9d319913d2000a92b7b7a6187b8b
João Benine <joaobenine@gmmail.com>
2022-11-18 02:29:39 -0300

    obtendo tempo total

--- src/script.js
+++ src/script.js
@@ -17,20 +17,20 @@
 
 
      get seconds() {
-         return this._diffOfTimes / 1000
+         return Math.floor(this._diffOfTimes / 1000)
      }
 
 
      get minutes() {
-         return this._diffOfTimes / (60 * 1000)
+         return Math.floor(this._diffOfTimes / (60 * 1000))
      }
 
      get hours() {
-         return this._diffOfTimes / (60 * 60 * 1000)
+         return Math.floor(this._diffOfTimes / (60 * 60 * 1000))
      }
 
      get days() {
-         return this._diffOfTimes / (24 * 60 * 60 * 1000)
+         return Math.floor(this._diffOfTimes / (24 * 60 * 60 * 1000))
      }
 
 
@@ -39,4 +39,4 @@
 
  const dataReferenciaCoutdown = new Countdown('24 December 2022 23:59:59 GMT-0300');
 
- console.log(dataReferenciaCoutdown._diffOfTimes);
+ console.log(dataReferenciaCoutdown.days);
