4cbfc91aa4b3453fa55d95126d092393d76e1d66
João Benine <joaobenine@gmmail.com>
2022-11-18 02:15:36 -0300

    pegando data atual

--- src/script.js
+++ src/script.js
@@ -3,10 +3,16 @@
          this.dataFutura = dataFutura;
      }
 
+
+     get _actualDate() {
+         return new Date();
+     }
+
      get _futureDate() {
-         return new(this._futureDate);
+         return new Date(this.dataFutura);
      }
  }
 
  const dataReferenciaCoutdown = new Countdown('24 December 2022 23:59:59 GMT-0300');
- console.log(dataReferenciaCoutdown);
+
+ console.log(dataReferenciaCoutdown._futureDate.getDay());
