49e9abb150018152c0efb9643e95485fd85903ad
João Benine <joaobenine@gmmail.com>
2022-11-18 02:22:33 -0300

    pegando conversão em dias/horas/minutos/segundos

--- src/script.js
+++ src/script.js
@@ -14,6 +14,27 @@
      get _diffOfTimes() {
          return this._futureDate.getTime() - this._actualDate.getTime();
      }
+
+
+     get seconds() {
+         return this._diffOfTimes / 1000
+     }
+
+
+     get minutes() {
+         return this._diffOfTimes / (60 * 1000)
+     }
+
+     get hours() {
+         return this._diffOfTimes / (60 * 60 * 1000)
+     }
+
+     get days() {
+         return this._diffOfTimes / (24 * 60 * 60 * 1000)
+     }
+
+
+
  }
 
  const dataReferenciaCoutdown = new Countdown('24 December 2022 23:59:59 GMT-0300');
